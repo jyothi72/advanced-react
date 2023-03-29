27abec7acacbfe2cf0a3887dcf077314ab5b9103
jyothi72 <jyothilax72@gmail.com>
2023-03-29 14:47:20 +0530

    add 100rs

--- src/Components/Expenseitem.js
+++ src/Components/Expenseitem.js
@@ -1,27 +1,30 @@
-import React from 'react'
+import React,{ useState }from 'react'
 import ExpenseDate from './ExpenseDate';
 import Card from './Card'
 import './Expenseitem.css'
 
 const ExpenseItem=(props)=> {
-  const change=()=>{
-    console.log('clicked')
-  }
-  const deleted=()=>{
-    console.log('deleted')
-  }
- 
- 
   
-    return (
+  const [title, setTitle]=useState(props.title)
+ const changed=()=>{
+  setTitle('updated')
+  console.log(title)
+}
+const[amount,newamount]=useState(props.amount)
+const changeamount=()=>{
+  newamount('100')
+}
+
+ return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className='expense-item__description'>
-        <h2 className='expense-item h2'>{props.title}</h2>
-        <div className='expense-item__price'>{props.amount}</div>
+        <h2 className='expense-item h2'>{title}</h2>
+        <div className='expense-item__price'>{amount}</div>
       </div>
-      <button onClick={change}>changetitle</button>
-      <button onClick={deleted}></button>
+      <button onClick={changeamount}>changamount</button>
+      <button onClick={changed}>changetitle</button>
+     
     </Card>
   );
 }
