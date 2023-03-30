6707647af603fe10bd53e99ee307c04b94583b3e
jyothi72 <jyothilax72@gmail.com>
2023-03-30 11:42:48 +0530

    adding state and consoling form

--- src/Components/NewExpense/Expenseform.js
+++ src/Components/NewExpense/Expenseform.js
@@ -1,19 +1,44 @@
+import React,{useState} from 'react'
 import "./Expenseform.css";
 const Expenseform = () => {
+  const [enteredtitle,setentered]=useState('');
+  const [enteredamount,setenteredamount]=useState('');
+  const [entereddate,setentereddate]=useState('');
+
+    const showtitle=(event)=>{
+      setentered(event.target.value)
+    }
+     const showamount=(event)=>{
+      setenteredamount(event.target.value)
+    }
+     const showdate=(event)=>{
+      setentereddate(event.target.value)
+    }
+    const submithandler=(event)=>{
+      event.preventDefault()
+      const expenseData={
+        title:enteredtitle,
+        amount:enteredamount,
+        date:new Date(entereddate)
+      }
+      console.log(expenseData)
+    }
+
+
   return (
-    <form>
+    <form onSubmit={submithandler}>
       <div className="controls">
         <div className="controls">
           <label className="labeljyo">title</label>
-          <input type={Text}  className="inputsel"/>
+          <input type='text'  className="inputsel" onChange={showtitle}/>
         </div>
         <div className="controls">
           <label className="labeljyo">amount</label>
-          <input type='number' className="inputsel" />
+          <input type='number' className="inputsel" onChange={showamount}/>
         </div>
         <div className="controls">
           <label className="labeljyo">Date</label>
-          <input type='date'  className="inputsel"/>
+          <input type='date'  className="inputsel" onChange={showdate}/>
         </div>
       </div>
       <div className="actions">
