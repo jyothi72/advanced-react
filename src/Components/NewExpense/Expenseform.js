b3dd6d39a79b973bb0a81334fc4f25c772caf82a
jyothi72 <jyothilax72@gmail.com>
2023-04-01 17:11:19 +0530

    added a cancel button and hided add expense form.

--- src/Components/NewExpense/Expenseform.js
+++ src/Components/NewExpense/Expenseform.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import "./Expenseform.css";
-const Expenseform = () => {
+const Expenseform = (props) => {
   const [enteredtitle,setentered]=useState('');
   const [enteredamount,setenteredamount]=useState('');
   const [entereddate,setentereddate]=useState('');
@@ -21,27 +21,29 @@ const Expenseform = () => {
         amount:enteredamount,
         date:new Date(entereddate)
       }
-      console.log(expenseData)
+      props.SaveExpensedata(expenseData)
+     setentered('')
+     setentereddate('')
+     setenteredamount('')
     }
-
-
-  return (
+ return (
     <form onSubmit={submithandler}>
       <div className="controls">
         <div className="controls">
           <label className="labeljyo">title</label>
-          <input type='text'  className="inputsel" onChange={showtitle}/>
+          <input type='text'   value={enteredtitle} className="inputsel" onChange={showtitle}/>
         </div>
         <div className="controls">
           <label className="labeljyo">amount</label>
-          <input type='number' className="inputsel" onChange={showamount}/>
+          <input type='number'   value={enteredamount}className="inputsel" onChange={showamount}/>
         </div>
         <div className="controls">
           <label className="labeljyo">Date</label>
-          <input type='date'  className="inputsel" onChange={showdate}/>
+          <input type='date'  value={entereddate} className="inputsel" onChange={showdate}/>
         </div>
       </div>
       <div className="actions">
+        <button type="button"className='jyoobutton' onClick={props.onCancel}>cancel</button>
         <button type="submit" className="jyoobutton">add expense</button>
       </div>
     </form>
--- src/Components/NewExpense/NewExpense.js
+++ src/Components/NewExpense/NewExpense.js
@@ -1,11 +1,27 @@
-
+import React,{useState}from 'react'
 import Expenseform from './Expenseform';
 import './newExpense.css'
 
-const NewExpense=()=>{
+const NewExpense=(props)=>{
+  const [edited,isediting]=useState(false)
+   const ExpensesaveHandler=(enteredExpensedata)=>{
+    const expensedatas={
+       ...enteredExpensedata
+        }
+      props.lastadd(expensedatas)
+      isediting(false)
+   }
+   const editinghandle=()=>{
+      isediting(true)
+   }
+   const stopedit=()=>{
+      isediting(false)
+   }
+
 return (
 <div className='newexpense'>
-<Expenseform/>
+   {!edited&&<button onClick={editinghandle}>addexpense</button>}
+{edited&&<Expenseform SaveExpensedata={ExpensesaveHandler} onCancel={stopedit}/>}
 </div>)
 }
 export default NewExpense
